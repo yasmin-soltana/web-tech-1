17877a9b6d7193297e071baa71b78e6da49f61b4
yasmin-soltana <yasminfeni1998@gmail.com>
2021-06-22 11:02:40 +0600

    first commit

---
+++ History.php
@@ -0,0 +1,24 @@
+
+
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Hitory</title>
+    </head>
+    <body>
+        <html>
+    <head>
+    <body>
+
+        <h1>Page 3 [History]</h1>
+        <h1>Conversion site</h1>
+        <p>Conversion history</p>
+
+         
+    1 .<a href="home.php">Home</a>
+    2 .<a href="rate.php">Conversion Rate</a>
+    3 .<a href="history.php">History</a>
+
+
+</body>
+</html
